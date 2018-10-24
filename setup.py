91e469e499493020d30d0a9dada5577bdc9139f7
brando <brancraft@gmail.com>
2018-10-24 09:02:07 -0400

    Using gte operator makes more sense

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup (
     version='0.1',
     packages=["paymentrails", "paymentrails.exceptions"],
     package_data={"paymentrails": ["ssl/*"]},
-    install_requires=['requests==2.13.0'],
+    install_requires=['requests>=2.13.0'],
     author='Jesse',
     author_email='jesse.silber@paymentrails.com',
     summary='Payment Rails Python SDK',

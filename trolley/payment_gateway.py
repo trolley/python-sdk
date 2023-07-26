d26454858642c274b9f6e53e99494490937943df
Aman Alam <shekh.aman@gmail.com>
2023-07-25 23:45:58 -0400

    Added payment.search methods and relevant tests

--- trolley/payment_gateway.py
+++ trolley/payment_gateway.py
@@ -1,7 +1,9 @@
 from collections import namedtuple
+import pprint
 from trolley.exceptions.invalidFieldException import InvalidFieldException
 import trolley.configuration
 
+from trolley.utils.meta import Meta
 
 class PaymentGateway(object):
     """
@@ -54,16 +56,56 @@ class PaymentGateway(object):
         trolley.configuration.Configuration.client(
             self.config).delete(endpoint)
         return True
-    def search(self, page=1, page_number=10, term=""):
-        endpoint = '/v1/batches?search=' + term + \
-            '&page=' + str(page) + '&pageSize=' + str(page_number)
+
+    """ Lists all payments under a batch.
+     This is basically an alias to the search() method. """
+    def list_all_payments(self, batch_id):
+        return self.search(batch_id)
+
+
+    """ Search payments within a batch, whose id is provided, with a search term.
+     This method returns a generator which auto paginates.
+     You can use this generator in a foreach loop to sequentially go through all the 
+      search results without needing to call this method again.
+       
+        For accessing specific pages, check the search_by_page() method. """
+    def search(self, batch_id, term=""):
+        page = 0
+        should_paginate = True
+        while should_paginate:
+            page+=1
+            endpoint = f'/v1/batches/{batch_id}/payments?search={term}'
+            params = f'&page={page}&pageSize=10'
+            response = trolley.configuration.Configuration.client(self.config).get(endpoint+params)
+            yield from self.__build_payments_from_response(response, False)
+
+            if page < response["meta"]["pages"]:
+                should_paginate = True
+            else:
+                should_paginate = False
+    
+    """ Search payments by providing a page number.
+     You should use this function when you want to paginate manually. """
+    def search_by_page(self, batch_id, term="", page=1, page_size=10):
+        endpoint = f'/v1/batches/{batch_id}/payments?search={term}'
+        params = f'&page={page}&pageSize={page_size}'
+        
         response = trolley.configuration.Configuration.client(
-            self.config).get(endpoint)
+            self.config).get(endpoint+params)
+        
+        return self.__build_payments_from_response(response, True)
+        
+    def __build_payments_from_response(self, response, include_meta=False):
         payments = []
         count = 0
-        for payment in response['batches']:
+        for payment in response['payments']:
             temppayment = trolley.payment.Payment.factory(payment)
             newpayment = namedtuple("Payment", temppayment.keys())(*temppayment.values())
             payments.insert(count, newpayment)
             count = count + 1
+        
+        if include_meta:
+            tempmeta = Meta.factory(response['meta'])
+            payments.insert(count,namedtuple("Meta", tempmeta.keys())(*tempmeta.values()))
+
         return payments

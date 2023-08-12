2602da4374906d95293a212c4d30a649c08aca5e
Aman Alam <shekh.aman@gmail.com>
2023-08-12 01:02:47 -0400

    Moved batch_summary.py to folder - types

--- trolley/__init__.py
+++ trolley/__init__.py
@@ -6,5 +6,5 @@ from trolley.batch import Batch
 from trolley.payment import Payment
 from trolley.recipient_account import RecipientAccount
 from trolley.gateway import Gateway
-from trolley.batch_summary import BatchSummary
+from trolley.types.batch_summary import BatchSummary
 from trolley.types.log import Log

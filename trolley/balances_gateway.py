083980f23a3cb44357e75a9e4039963fb7448c93
Aman Alam <shekh.aman@gmail.com>
2023-08-14 23:46:49 -0400

    Covered missing Balances API, added relevant tests

--- trolley/balances_gateway.py
+++ trolley/balances_gateway.py
@@ -5,20 +5,29 @@ import trolley.configuration
 
 class BalancesGateway(object):
     """
-    PaymentRails Balance module
-    Manages balances
+    Trolley Balance module
     """
 
     def __init__(self, gateway, config):
         self.gateway = gateway
         self.config = config
 
-    def find(self, term=""):
+    def get_trolley_balance(self):
+        return self.get_all_balances("paymentrails")
+    
+    def get_paypal_balance(self):
+        return self.get_all_balances("paypal")
+
+    def get_all_balances(self, term=""):
         if term is None:
             raise InvalidFieldException("Term cannot be None")
         endpoint = '/v1/balances/' + term
         response = trolley.configuration.Configuration.client(
             self.config).get(endpoint)
-        oldbalance = trolley.balances.Balances.factory(response)
-        balance = namedtuple("Balances", oldbalance.keys())(*oldbalance.values())
-        return balance
+        
+        balances = []
+        for balance in response['balances']:
+            tempbalance = trolley.Balances.factory(balance)
+            balances.append(namedtuple("Balances", tempbalance.keys())(*tempbalance.values()))
+
+        return balances
